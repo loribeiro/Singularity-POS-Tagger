4cdae53ebdd1bed21c5c160286c807867f9ec928
Lucas Ribeiro <l.oribeiro@outlook.com>
2021-02-11 16:08:37 -0300

    treat nonexistent word

--- normalize_text/normalize_string/normalizeString.js
+++ normalize_text/normalize_string/normalizeString.js
@@ -3,12 +3,15 @@ const removerAcentos = require("./lib/remove_acentos")
 function removerAcentosTexto(chunk){
   const lista_palavras = chunk.replace(/\r?\n|\r/g," ").split(" ")
   var texto_tratado = ""
-
+  
   for(let i =0; i<lista_palavras.length; i++){
       let palavra = lista_palavras[i].split("_")[0]
+      if (palavra === ""){
+      }else{
+        texto_tratado = (texto_tratado === "" ? "" : (texto_tratado + " ") ) 
+                          + removerAcentos(palavra)
+      }
       
-      texto_tratado = (texto_tratado === "" ? "" : (texto_tratado + " ") ) 
-                        + removerAcentos(palavra)
   }
   
   
