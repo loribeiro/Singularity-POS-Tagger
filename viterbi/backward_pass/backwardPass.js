7b77ae09b75e5019418d3e9b1a2c5dc0f66f3f09
Lucas Ribeiro <36322804+loribeiro@users.noreply.github.com>
2021-03-07 18:40:04 -0300

    refactoring variable names

--- viterbi/backward_pass/backwardPass.js
+++ viterbi/backward_pass/backwardPass.js
@@ -7,21 +7,21 @@ async function backwardPass(word, tags, matC, matD){
         return new Promise(function(resolve, reject){
             let max = -Infinity
             let index = 0
-            let actualPosition = words.length -1
+            let currentlyPosition = words.length -1
             for(let i = 0; i<tagsVector.length; i++){
                 if(matrixC[i][words.length-1]> max){
                     max = matrixC[i][words.length-1]
                     index = i
                 }
             }
-            let saida = []
-            saida.unshift(words[actualPosition]+"_"+tagsVector[index])
-            while(actualPosition>0){
-                index = matrixD[index][actualPosition]
-                actualPosition = actualPosition - 1
-                saida.unshift(words[actualPosition]+"_"+tagsVector[index])
+            let output = []
+            output.unshift(words[currentlyPosition]+"_"+tagsVector[index])
+            while(currentlyPosition>0){
+                index = matrixD[index][currentlyPosition]
+                currentlyPosition = currentlyPosition - 1
+                output.unshift(words[currentlyPosition]+"_"+tagsVector[index])
             }
-            resolve(saida)
+            resolve(output)
 
         })
        
